d52e67d9430f661daca3e4e5546fee4d70138061
Siam10101 <mehedihasansiam030@gmail.com>
2023-05-05 23:26:07 +0600

    some changes chef json file

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 // const cors = require('cors');
-const cors = require('cors')
+const cors = require("cors");
 
-const chef = require('./data/chef.json')
-const recipes = require('./data/recipes.json')
+const chef = require("./data/chef.json");
+const recipes = require("./data/recipes.json");
 
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hy Hello World!");
+});
 
-
-app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/chef', (req, res) => {
-  res.send(chef)
-})
-
-
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
 
 // app.get('/recipes', (req, res) => {
 //   res.send(recipes)
 // })
 
-
-
-app.get('/chef/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const selectChef = recipes.filter(n => parseInt(n.id) === id)
-  res.send(selectChef)
-})
-
-
+app.get("/chef/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectChef = recipes.filter((n) => parseInt(n.id) === id);
+  res.send(selectChef);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
